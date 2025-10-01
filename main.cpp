37c185b53f4234a8533e16da95766c900789d244
G.kai <gildokai781@gmail.com>
2025-10-01 10:45:47 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+int main()
+{
+
+   
+int p1,p2,p3,p4,media;
+std::cout<<"escrever fisica";
+std::cin>>p1;
+std::cout<<"escrever matematica";
+std::cin>>p2;
+std::cout<<"escrever quimica";
+std::cin>>p3;
+std::cout<<"escrever ingles",
+std::cin>>p4;
+
+media=(p1+p2+p3+p4)/4;
+std::cout<<"a media dos quatro numeros é"<<media<<"!!!"; 
+return 0;
+}
