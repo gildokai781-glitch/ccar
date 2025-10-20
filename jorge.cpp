7d0bac0fc91cd2c416561f09db5b35fd4beded69
G.KAI <gildokai781@gmail.com>
2025-10-20 09:26:15 +0100

    Add files via upload

---
+++ jorge.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <iostream>
+#include <locale.h>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "portuguese");
+	int num, i, r;
+	for (int i < 10);
+	{
+cout<<
+	}
+
+	
